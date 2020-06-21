633786443699d7aa44bc6d5210997082bcc37b8e
webdbmike12345 <webdbmike@gmail.com>
2020-06-20 22:43:19 -0500

    create react redux setup

--- src/App.js
+++ src/App.js
@@ -1,20 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Provider } from 'react-redux';
+import store from './redux/store';
 import './App.css';
 
 const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          React test new script<code>src/App.js</code> and save to reload.
-        </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <div className="app">
+        <h1>Setup React Redux</h1>
+      </div>
+    </Provider>
   );
 };
 
