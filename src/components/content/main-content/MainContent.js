f847f63309a90f8e250ef2d670278a50fddd9f22
webdbmike12345 <webdbmike@gmail.com>
2020-06-27 01:46:47 -0500

    grid component styles

--- src/components/content/main-content/MainContent.js
+++ src/components/content/main-content/MainContent.js
@@ -1,17 +1,33 @@
 import React from 'react';
 import './MainContent.scss';
 import Slideshow from '../slide-show/Slideshow';
+import Grid from '../grid/Grid';
 
 const MainContent = () => {
   const images = [
     {
-      url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTAcR87zzk3AgOkFp5VeA6GTJuMkUpnSIi76A&usqp=CAU'
+      url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTAcR87zzk3AgOkFp5VeA6GTJuMkUpnSIi76A&usqp=CAU',
+      rating: 7.5
     },
     {
-      url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSOFiId-5zxEzIIYl0SdcAdhGaHhF46X3hEkQ&usqp=CAU'
+      url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTAcR87zzk3AgOkFp5VeA6GTJuMkUpnSIi76A&usqp=CAU',
+      rating: 8.5
     },
     {
-      url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTGY4huDEZxHuJE5Bh2eK0FRz2r3NxoTfccJA&usqp=CAU'
+      url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSOFiId-5zxEzIIYl0SdcAdhGaHhF46X3hEkQ&usqp=CAU',
+      rating: 7.8
+    },
+    {
+      url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSOFiId-5zxEzIIYl0SdcAdhGaHhF46X3hEkQ&usqp=CAU',
+      rating: 9.7
+    },
+    {
+      url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSOFiId-5zxEzIIYl0SdcAdhGaHhF46X3hEkQ&usqp=CAU',
+      rating: 6.5
+    },
+    {
+      url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTGY4huDEZxHuJE5Bh2eK0FRz2r3NxoTfccJA&usqp=CAU',
+      rating: 8.5
     }
   ];
   return (
@@ -21,7 +37,7 @@ const MainContent = () => {
         <div className="movieType">Now Playing</div>
         <div className="paginate">Paginate</div>
       </div>
-      {/* Display grid component */}
+      <Grid images={images}></Grid>
     </div>
   );
 };
