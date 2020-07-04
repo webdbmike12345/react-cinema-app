df10c9567763ffa46de9a17e402ef6e42b9fbfcb
webdbmike12345 <webdbmike@gmail.com>
2020-07-04 10:29:54 -0500

    Add detail component

--- src/App.js
+++ src/App.js
@@ -4,14 +4,21 @@ import Header from './components/header/Header';
 import store from './redux/store';
 import './App.scss';
 import Main from './components/main/Main';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Details from './components/content/details/Details';
 
 const App = () => {
   return (
     <Provider store={store}>
-      <Header></Header>
-      <div className="app">
-        <Main></Main>
-      </div>
+      <Router>
+        <Header></Header>
+        <div className="app">
+          <Switch>
+            <Route exact path="/" component={Main} />
+            <Route exact path="/:id/:name/details" component={Details} />
+          </Switch>
+        </div>
+      </Router>
     </Provider>
   );
 };
